ac52c942564f28cb41b18e29b44e7104439b68da
luizkmr <35347088+luizkmr@users.noreply.github.com>
2025-04-06 17:30:42 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import FormWizard from './components/FormWizard.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FormWizard />
   </React.StrictMode>
 )
